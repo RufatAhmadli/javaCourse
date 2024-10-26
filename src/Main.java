3308ccf786847704c6c40bbf9688eb0166d2bd07
user <ahmadlirufat10@gmail.com>
2024-10-26 20:48:56 +0400

    isNegative code added

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,10 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int a=new Scanner(System.in).nextInt();
+        boolean isNegative=a<0;
+        System.out.println(isNegative);
 
     }
 }
